11c780816f16959259c22eea995da15a6ecc4521
smaskit <smaskit@3eeecb2b-694e-4ba1-abfa-b9905e13d602>
2010-04-18 00:38:58 +0000

    add tpl files used by holds list
    git-svn-id: http://dobby.darienlibrary.org/svn/sopac/trunk@138 3eeecb2b-694e-4ba1-abfa-b9905e13d602

---
+++ sopac-user-holds-list-multirow.tpl.php
@@ -0,0 +1,47 @@
+<?php print $required_hidden_fields ?>
+<table cellspacing="0" class="sticky-enabled sticky-table" id="patroninfo">
+  <thead class="tableHeader-processed">
+    <tr>
+      <th>Delete</th>
+      <th>&nbsp;</th>
+      <th>&nbsp;</th>
+    </tr>
+  </thead>
+  <tbody>
+<?php
+  $zebra = 'even';
+  foreach ($holds as $hold) {
+  	$zebra = $zebra == 'odd' ? 'even' : 'odd';
+?>
+    <tr class="<?php print $zebra ?>">
+      <td><?php print $hold['cancel'] ?></td>
+      <td colspan="2"><?php print $hold['title_link'] ?></td>
+    </tr>
+    <tr class="<?php print $zebra ?>">
+      <td>&nbsp;</td>
+      <td>Availability:</td>
+      <td><?php print $hold['status'] ?> in queue</td>
+    </tr>
+    <tr class="<?php print $zebra ?>">
+      <td>&nbsp;</td>
+      <td>Pickup at:</td>
+      <td><?php print $hold['pickup'] ?></td>
+    </tr>
+    <tr class="<?php print $zebra ?>">
+      <td>&nbsp;</td>
+      <td>Status:</td>
+      <td><?php print $hold['freeze'] ?></td>
+    </tr>
+    <tr class="<?php print $zebra ?>">
+      <td>&nbsp;</td>
+      <td>Suspended:</td>
+      <td><?php print $hold['suspend_from'] ?><?php print $hold['suspend_to'] ?></td>
+    </tr>
+<?php
+  }
+?>
+    <tr class="profile_button <?php print $zebra ?>">
+      <td colspan="3"><?php print $submit ?></td>
+    </tr>
+  </tbody>
+</table>
---
+++ sopac-user-holds-list.tpl.php
@@ -0,0 +1,46 @@
+<?php print $required_hidden_fields ?>
+<table cellspacing="0" class="sticky-enabled sticky-table" id="patroninfo">
+  <thead class="tableHeader-processed">
+    <tr>
+    <?php if ($freezes_enabled) { ?>
+      <th>Delete</th>
+    <?php } ?>
+      <th>Title</th>
+      <th>Status</th>
+      <th>Pickup Location</th>
+    <?php if ($freezes_enabled) { ?>
+      <th>Freeze</th>
+    <?php } ?>
+    </tr>
+  </thead>
+  <tbody>
+<?php
+  $zebra = 'even';
+  foreach ($holds as $hold) {
+  	$zebra = $zebra == 'odd' ? 'even' : 'odd';
+?>
+    <tr class="<?php print $zebra ?>">
+    <?php if ($freezes_enabled) { ?>
+      <td><?php print $hold['cancel'] ?></td>
+    <?php } ?>
+      <td><?php print $hold['title_link'] ?></td>
+      <td><?php print $hold['status'] ?> in queue</td>
+      <td><?php print $hold['pickup'] ?></td>
+    <?php if ($freezes_enabled) { ?>
+      <td><?php print $hold['freeze'] ?></td>
+    <?php } ?>
+    </tr>
+<?php
+  }
+?>
+    <tr class="profile_button <?php print $zebra ?>">
+    <?php if ($freezes_enabled) { ?>
+      <td colspan="5">
+    <?php } else { ?>
+      <td colspan="3">
+    <?php } ?>
+        <?php print $submit ?>
+      </td>
+    </tr>
+  </tbody>
+</table>
