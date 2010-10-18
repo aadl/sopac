c8e1043df4e78047773c0d579fd5bd2bfab46ce5
Ryan Eby <ryaneby@gmail.com>
2010-10-18 09:35:47 -0400

    removed checkbox for art print renewals

--- sopac_user.php
+++ sopac_user.php
@@ -273,7 +273,7 @@ function sopac_user_chkout_table(&$account, &$locum, $max_disp = NULL) {
         }
         $author = l($new_author_str, variable_get('sopac_url_prefix', 'cat/seek') . '/search/author/' . urlencode($new_author_str));
         if ($co['avail']['holds'] == 0 &&
-            $co['bib']['mat_code'] != 's' &&
+            $co['bib']['mat_code'] != 's' && $co['bib']['mat_code'] != 'p' &&
             !($co['ill'] == 1 && $co['numrenews'] > 0)) {
           $checkbox = '<input type="checkbox" name="inum[' . $co['inum'] . ']" value="' . $co['varname'] . '">';
         }
@@ -304,18 +304,11 @@ function sopac_user_chkout_table(&$account, &$locum, $max_disp = NULL) {
   // Wrap it together inside a form
   $content = '<form method="post">' . theme('table', $header, $rows, array('id' => 'patroninfo', 'cellspacing' => '0')) . '</form>';
   $token = $locum->get_token($account->uid);
-  if (!$token) {
+  if(!$token){
    $token = $locum->set_token($account->uid);
   }
-  if ($token) {
-    $image_path = base_path() . path_to_theme() . '/images';
-    $content .= '<p>';
-    $content .= l('<img src="' . $image_path . '/feed.png" alt="Syndicate your Checkouts" />',
-                  "http://api.aadl.org/user/checkouts?token=$token", array('html' => TRUE));
-    $content .= '&nbsp;';
-    $content .= l('<img src="' . $image_path . '/ical.png" alt="iCal feed for Checkouts" />',
-                  "webcal://api.aadl.org/user/ical?token=$token", array('html' => TRUE));
-    $content .= '</p>';
+  if($token){
+    $content .= '<p><a href="http://api.aadl.org/user/checkouts?token='.$token.'"><img src="https://www.aadl.org/sites/default/themes/zen/aadl/images/feed.png" alt = "Syndicate Your Checkouts" /> <a href="ical://api.aadl.org/user/ical?token='.$token.'"><img src="https://www.aadl.org/sites/default/themes/zen/aadl/images/ical.png" alt = "iCal feed for Checkouts" /></a></p>';
   }
 
   return $content;
